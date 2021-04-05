1b2bdcdf106eec800f1b0335709f7f4ba8ab4c00
muhammadnafaaditya <nafaaditya5@gmail.com>
2021-04-04 21:03:56 -0700

    coding pertama

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	   <title>kalkulator sederhana</title>
+</head>
+<body>
+      <fore action="proses.php" method="post">
+           <input type="text" name="angka1">
+           <input type="text" name="angka2">
+           <input type="submit" name="tambah" value="tambah">
+          </form>
+    </body>
+</html>
---
+++ proses.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+ <html>
+    <head>
+	   <title>kalkulator sederhana</title>
+ </head>
+ <body>
+       <fore action="proses.php" method="post">
+       	   <input type="text" name="angka1">
+       	   <input type="text" name="angka2">
+       	   <input type="submit" name="tambah" value="tambah">
+       	</fore>
+      </body> 	   
